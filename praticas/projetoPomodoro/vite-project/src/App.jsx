8439d24d9ff079f4b1aaf06da34c3cebfc2d20d2
AlanVinicius357 <alanvinicius295@gmail.com>
2026-03-24 21:18:18 -0300

    realização das praticas 4 até 8

--- praticas/projetoPomodoro/vite-project/src/App.jsx
+++ praticas/projetoPomodoro/vite-project/src/App.jsx
@@ -2,12 +2,41 @@
 import './styles/theme.css';
 // 2º: Importamos os estilos globais
 import './styles/global.css';
+//importação do heading
+import { Heading } from './components/Heading';
+// Importando o ícone
+import { Timer } from 'lucide-react'; 
 
 export function App() {
   return (
     <>
-      <h1>Olá, Mundo!</h1>
-      <p>Testando o CSS Global.</p>
+      {/* Seção 1: Logo */}
+      <div className='container'>
+        <div className='content'>
+          <p>Logo do App</p>
+        </div>
+      </div>
+
+      {/* Seção 2: Menu */}
+      <div className='container'>
+        <div className='content'>
+          <p>Menu de Navegação</p>
+        </div>
+      </div>
+
+      {/* Seção 3: Formulário / Cronômetro */}
+      <div className='container'>
+        <div className='content'>
+          <p>Área do Cronômetro</p>
+        </div>
+      </div>
+
+      {/* Seção 4: Footer */}
+      <div className='container'>
+        <div className='content'>
+          <p>Rodapé da Página</p>
+        </div>
+      </div>
     </>
   );
 }
---
+++ praticas/projetoPomodoro/vite-project/src/components/Heading.tsx
@@ -0,0 +1,13 @@
+
+import styles from './Heading.module.css';
+
+
+import React from 'react'; 
+type HeadingProps = {
+  
+  children: React.ReactNode;
+};
+
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
+}
