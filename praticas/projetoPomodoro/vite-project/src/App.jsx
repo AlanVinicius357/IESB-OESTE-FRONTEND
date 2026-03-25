0e66bdecbe932f7538559e162f8fdad7c3b7d072
AlanVinicius357 <alanvinicius295@gmail.com>
2026-03-24 21:19:23 -0300

    a

--- praticas/projetoPomodoro/vite-project/src/App.jsx
+++ praticas/projetoPomodoro/vite-project/src/App.jsx
@@ -9,34 +9,11 @@ import { Timer } from 'lucide-react';
 
 export function App() {
   return (
-    <>
-      {/* Seção 1: Logo */}
-      <div className='container'>
-        <div className='content'>
-          <p>Logo do App</p>
-        </div>
-      </div>
-
-      {/* Seção 2: Menu */}
-      <div className='container'>
-        <div className='content'>
-          <p>Menu de Navegação</p>
-        </div>
-      </div>
-
-      {/* Seção 3: Formulário / Cronômetro */}
-      <div className='container'>
-        <div className='content'>
-          <p>Área do Cronômetro</p>
-        </div>
-      </div>
-
-      {/* Seção 4: Footer */}
-      <div className='container'>
-        <div className='content'>
-          <p>Rodapé da Página</p>
-        </div>
-      </div>
-    </>
+    <Heading>
+      Histórico
+      <button>
+        <Timer /> {/* Renderizando o ícone dentro do botão */}
+      </button>
+    </Heading>
   );
 }
